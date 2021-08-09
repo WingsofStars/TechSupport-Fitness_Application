fe593e3dfba71ca71aa1314ab156f9887294b922
WingsofStars <78242053+WingsofStars@users.noreply.github.com>
2021-08-09 16:26:32 -0600

    Added a run on first boot create user Activity Updated the new User Screen to "create user" Version - 1.02

--- app/src/main/java/delware/apps/techsupport_scampermobile/MainActivity.java
+++ app/src/main/java/delware/apps/techsupport_scampermobile/MainActivity.java
@@ -3,15 +3,36 @@ package delware.apps.techsupport_scampermobile;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+    public SharedPreferences prefs; // uses small save files know as "Shared Prefrences"
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+
+        prefs = getSharedPreferences("prefs", MODE_PRIVATE);
+
+//        boolean firstStart = prefs.getBoolean("isThisFirstStart", true);// Searches for a Shared  Preferences Value, if one doesn't exists, it is created with the default value of true
+//
+//        if (firstStart){
+//
+//            SharedPreferences.Editor editor = prefs.edit(); // Makes an editor to change the values of a shared Preferences
+//            editor.putBoolean("isThisFirstStart", false);//Changes Value to False
+//            editor.apply();
+//        }
+
+
+        boolean doesUserExist = prefs.getBoolean("Exists", false); //Checks for user account if it doesn't exists, it creates a SP(Shared Preference) saying it Doesn't
+        if(!doesUserExist){
+            Intent goToCreateUser = new Intent(getApplicationContext(), newUserScreen.class);
+            startActivity(goToCreateUser); // Got to the create user Screen
+        }
     }
 
 
