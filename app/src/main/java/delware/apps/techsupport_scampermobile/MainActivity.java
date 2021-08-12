bb0c69104c823dd954c6660f624ca5f5a7cb2f16
WingsofStars <78242053+WingsofStars@users.noreply.github.com>
2021-08-12 15:32:38 -0600

    Version - 1.10 Nothing Much

--- app/src/main/java/delware/apps/techsupport_scampermobile/MainActivity.java
+++ app/src/main/java/delware/apps/techsupport_scampermobile/MainActivity.java
@@ -1,5 +1,6 @@
 package delware.apps.techsupport_scampermobile;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
@@ -7,9 +8,17 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.EditText;
+import android.widget.PopupMenu;
+import android.widget.PopupWindow;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-    public SharedPreferences prefs; // uses small save files know as "Shared Prefrences"
+    static SharedPreferences prefs; // uses small save files know as "Shared Preferences"
+    public AlertDialog.Builder dBuilder;
+    public AlertDialog dialogue;
     static TextView TV;
 
     @Override
@@ -31,10 +40,9 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
 
-        boolean doesUserExist = prefs.getBoolean("Exists", false); //Checks for user account if it doesn't exists, it creates a SP(Shared Preference) saying it Doesn't
-        if(!doesUserExist){
-            Intent goToCreateUser = new Intent(getApplicationContext(), newUserScreen.class);
-            startActivity(goToCreateUser); // Got to the create user Screen
+        boolean isUserLoggedIn = prefs.getBoolean("LoggedIn", false); //Checks for user account if it doesn't exists, it creates a SP(Shared Preference) saying it Doesn't
+        if(!isUserLoggedIn){
+            openLoginScreen();
         }
     }
 
@@ -52,6 +60,39 @@ public class MainActivity extends AppCompatActivity {
     public void goToTrackingScreen(View v){
         Intent goToSettings = new Intent(MainActivity.this, trackingScreen.class);
         startActivity(goToSettings);
+    }
+    public void goToNewUserScreen(View v){
+        Intent goToCreateUser = new Intent(getApplicationContext(), newUserScreen.class);
+        startActivity(goToCreateUser); // Got to the create user Screen
+    }
+
+
+    public void openLoginScreen(){
+        dBuilder = new AlertDialog.Builder(this);
+        final View loginPopup = getLayoutInflater().inflate(R.layout.loginpopup, null);
+        dBuilder.setView(loginPopup);
+        dialogue = dBuilder.create();
+        dialogue.show();
+    }
+
+    public void attemptLogin(View V){
+        EditText givenUsernameView = findViewById(R.id.userNameText);
+        EditText givenPasswordView = findViewById(R.id.passwordText);
+
+        String givenUserName = givenUsernameView.getText().toString();
+        String givenPassword = givenPasswordView.getText().toString();
+
+        //Run these given values through a sql query
+
+        int id = 0;
+
+
+        //When everything is logged in:
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.putString("Username", givenUserName);
+//        editor.putInt("UserId", id);
+//        editor.putString("Password", givenPassword);//needs to be scrambled
+//        editor.putBoolean("LoggedIn", true);
     }
 
     public void goToCollectionScreen(View v){
