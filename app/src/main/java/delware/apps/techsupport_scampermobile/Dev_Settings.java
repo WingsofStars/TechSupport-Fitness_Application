4c681e3a2617aa433df30e6f1ba84d65994e57e7
WingsofStars <78242053+WingsofStars@users.noreply.github.com>
2021-08-12 14:52:16 -0600

    Version - 1.06 Updated the settings screen with a list view

---
+++ app/src/main/java/delware/apps/techsupport_scampermobile/Dev_Settings.java
@@ -0,0 +1,13 @@
+package delware.apps.techsupport_scampermobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Dev_Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dev_settings);
+    }
+}
---
+++ app/src/main/java/delware/apps/techsupport_scampermobile/Profile_Settings.java
@@ -0,0 +1,14 @@
+package delware.apps.techsupport_scampermobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Profile_Settings extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile_settings);
+    }
+}
