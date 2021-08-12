a6c70f9dec908f76e58922fc743072e4edbc709c
ColeBelfry <cbelfry@student.neumont.edu>
2021-08-12 12:49:03 -0600

    Fixed some stuff

--- app/src/main/java/delware/apps/techsupport_scampermobile/DBHandler.java
+++ app/src/main/java/delware/apps/techsupport_scampermobile/DBHandler.java
@@ -13,6 +13,8 @@ public class DBHandler extends SQLiteOpenHelper{
     private static final String KEY_ID = "UserId";
     private static final String KEY_NAME = "Username";
     private static final String KEY_PASSWORD = "Password";
+    private static final String KEY_WEIGHT = "Weight";
+    private static final String KEY_HEIGHT = "Height";
     private static final String KEY_lEVEL = "Level";
     public DBHandler(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
@@ -22,7 +24,8 @@ public class DBHandler extends SQLiteOpenHelper{
     //creates the user database
     public void onCreate(SQLiteDatabase db) {
         String CREATE_USER_TABLE = "CREATE TABLE " + TABLE_USERS + "(" + KEY_ID + " INTEGER PRIMARY KEY AUTOINCREMENT, "
-                + KEY_NAME + " TEXT, " + KEY_PASSWORD + "TEXT, " + KEY_lEVEL + "INTEGER);";
+                + KEY_NAME + " TEXT, " + KEY_PASSWORD + "TEXT, " + KEY_WEIGHT + "TEXT, " + KEY_HEIGHT + "TEXT, "
+                + KEY_lEVEL + "INTEGER);";
         db.execSQL(CREATE_USER_TABLE);
     }
 
@@ -39,6 +42,8 @@ public class DBHandler extends SQLiteOpenHelper{
         ContentValues values = new ContentValues();
         values.put(KEY_NAME, profile.getUserName());
         values.put(KEY_PASSWORD, profile.getPassword());
+        values.put(KEY_WEIGHT, profile.getWeight());
+        values.put(KEY_HEIGHT, profile.getHeight());
         values.put(KEY_lEVEL, profile.getLevel());
         //Inserts table columns
         db.insert(TABLE_USERS, null, values);
@@ -46,11 +51,14 @@ public class DBHandler extends SQLiteOpenHelper{
         db.close();
     }
 
-    public void getUser(int id){
+    public Profile getUser(int id){
         SQLiteDatabase db = this.getWritableDatabase();
-        Cursor cursor = db.query(TABLE_USERS, new String[] {KEY_ID,KEY_NAME,KEY_PASSWORD,KEY_lEVEL}, KEY_ID + "=?",
+        Cursor cursor = db.query(TABLE_USERS, new String[] {KEY_ID,KEY_NAME,KEY_PASSWORD,KEY_WEIGHT,KEY_HEIGHT,KEY_lEVEL}, KEY_ID + "=?",
                 new String[] { String.valueOf(id) }, null, null, null, null);
         if (cursor != null)
             cursor.moveToFirst();
+        Profile user = new Profile(Integer.parseInt(cursor.getString(0)),cursor.getString(1),cursor.getString(2),
+                cursor.getString(3),cursor.getString(4),Integer.parseInt(cursor.getString(5)));
+        return user;
     }
 }
