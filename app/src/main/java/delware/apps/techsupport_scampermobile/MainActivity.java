4b93e05c7426b7b359cee41c8bbe5afefa6a5722
WingsofStars <78242053+WingsofStars@users.noreply.github.com>
2021-08-15 05:09:28 -0600

    Version - 1.12 Test Push

--- app/src/main/java/delware/apps/techsupport_scampermobile/MainActivity.java
+++ app/src/main/java/delware/apps/techsupport_scampermobile/MainActivity.java
@@ -1,25 +1,36 @@
 package delware.apps.techsupport_scampermobile;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
-import delware.apps.techsupport_scampermobile.Screens.cardioDisplayScreen;
 import delware.apps.techsupport_scampermobile.Screens.newUserScreen;
 import delware.apps.techsupport_scampermobile.Screens.settings;
 import delware.apps.techsupport_scampermobile.Screens.trackingScreen;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    public SharedPreferences prefs; // uses small save files know as "Shared Prefrences"
-
+    static SharedPreferences prefs; // uses small save files know as "Shared Preferences"
+    public AlertDialog.Builder dBuilder;
+    public AlertDialog dialogue;
+    static TextView TV;
+    static String currerntID;
+    public static DBHandler databaseHandler;
+    static ArrayList<RunLog> RunLogs = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home);
+
+        databaseHandler = new DBHandler(MainActivity.this);
+        TV = findViewById(R.id.xpBar);
 
 
         prefs = getSharedPreferences("prefs", MODE_PRIVATE);
@@ -34,11 +45,11 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
 
-        boolean doesUserExist = prefs.getBoolean("Exists", false); //Checks for user account if it doesn't exists, it creates a SP(Shared Preference) saying it Doesn't
-        if(!doesUserExist){
-            Intent goToCreateUser = new Intent(getApplicationContext(), newUserScreen.class);
-            startActivity(goToCreateUser); // Got to the create user Screen
+        boolean isUserLoggedIn = prefs.getBoolean("LoggedIn", false); //checks for user account if it doesn't exists, it creates a SP(Shared Preference) saying it Doesn't
+        if(!isUserLoggedIn){
+            openLoginScreen();
         }
+        currerntID = String.valueOf(prefs.getInt("id", 0));
     }
 
 
@@ -55,5 +66,43 @@ public class MainActivity extends AppCompatActivity {
     public void goToTrackingScreen(View v){
         Intent goToSettings = new Intent(MainActivity.this, trackingScreen.class);
         startActivity(goToSettings);
+    }
+    public void goToNewUserScreen(View v){
+        Intent goToCreateUser = new Intent(getApplicationContext(), newUserScreen.class);
+        startActivity(goToCreateUser); // Got to the create user Screen
+    }
+
+
+    public void openLoginScreen(){
+        dBuilder = new AlertDialog.Builder(this);
+        final View loginPopup = getLayoutInflater().inflate(R.layout.loginpopup, null);
+        dBuilder.setView(loginPopup);
+        dialogue = dBuilder.create();
+        dialogue.show();
+    }
+
+    public void attemptLogin(View V){
+        EditText givenUsernameView = findViewById(R.id.userNameText);
+        EditText givenPasswordView = findViewById(R.id.passwordText);
+
+        String givenUserName = givenUsernameView.getText().toString();
+        String givenPassword = givenPasswordView.getText().toString();
+
+        //Run these given values through a sql query
+
+        int id = 0;
+
+
+        //When everything is logged in:
+//        SharedPreferences.Editor editor = prefs.edit();
+//        editor.putString("Username", givenUserName);
+//        editor.putInt("UserId", id);
+//        editor.putString("Password", givenPassword);//needs to be scrambled
+//        editor.putBoolean("LoggedIn", true);
+    }
+
+    public void goToCollectionScreen(View v){
+        Intent goToCollection = new Intent(MainActivity.this, stickerWallScreen.class);
+        startActivity(goToCollection);
     }
 }
