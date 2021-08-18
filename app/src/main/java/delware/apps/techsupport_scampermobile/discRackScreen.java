5688356e77c412f927a4a5cfb74a0c0df414c9f5
Joseph Baker <jbaker@student.neumont.edu>
2021-08-17 20:57:18 -0600

    User collection update, both sticker wall and disc rack are visually set up and ready to be used.

--- app/src/main/java/delware/apps/techsupport_scampermobile/discRackScreen.java
+++ app/src/main/java/delware/apps/techsupport_scampermobile/discRackScreen.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class discRackScreen extends AppCompatActivity {
+    private TextView TV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection_disc_rack);
+        TV = findViewById(R.id.DescriptionText2);
+        TV.setText("Tap on a disc to learn more...");
     }
 
     public void exitIntent(){
@@ -28,5 +32,25 @@ public class discRackScreen extends AppCompatActivity {
     public void goToStickerWallScreen(View v){
         Intent goToStickerWall = new Intent(discRackScreen.this, stickerWallScreen.class);
         startActivity(goToStickerWall);
+    }
+
+    public void showDiscDescription(View v) {
+        switch(v.getId()) {
+            case R.id.D01:
+                TV.setText("Disc 1 description...");
+                break;
+            case R.id.D02:
+                TV.setText("Disc 2 description...");
+                break;
+            case R.id.D11:
+                TV.setText("Disc 3 description...");
+                break;
+            case R.id.D12:
+                TV.setText("Disc 4 description...");
+                break;
+            default:
+                TV.setText("Invalid disc, tap to select another one...");
+                break;
+        }
     }
 }
--- app/src/main/java/delware/apps/techsupport_scampermobile/stickerWallScreen.java
+++ app/src/main/java/delware/apps/techsupport_scampermobile/stickerWallScreen.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class stickerWallScreen extends AppCompatActivity {
+    private TextView TV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_collection_sticker_wall);
+        TV = findViewById(R.id.DescriptionText1);
+        TV.setText("Tap on a sticker to learn more...");
     }
 
     public void exitIntent(){
@@ -28,5 +32,73 @@ public class stickerWallScreen extends AppCompatActivity {
     public void goToDiscRackScreen(View v){
         Intent goToDiscRack = new Intent(stickerWallScreen.this, discRackScreen.class);
         startActivity(goToDiscRack);
+    }
+
+    public void showStickerDescription(View v) {
+        switch(v.getId()) {
+            case R.id.A01:
+                TV.setText("1st");
+                break;
+            case R.id.A02:
+                TV.setText("2nd");
+                break;
+            case R.id.A03:
+                TV.setText("3rd");
+                break;
+            case R.id.A04:
+                TV.setText("4th");
+                break;
+            case R.id.A11:
+                TV.setText("5th");
+                break;
+            case R.id.A12:
+                TV.setText("6th");
+                break;
+            case R.id.A13:
+                TV.setText("7th");
+                break;
+            case R.id.A14:
+                TV.setText("8th");
+                break;
+            case R.id.A21:
+                TV.setText("9th");
+                break;
+            case R.id.A22:
+                TV.setText("10th");
+                break;
+            case R.id.A23:
+                TV.setText("11th");
+                break;
+            case R.id.A24:
+                TV.setText("12th");
+                break;
+            case R.id.A31:
+                TV.setText("13th");
+                break;
+            case R.id.A32:
+                TV.setText("14th");
+                break;
+            case R.id.A33:
+                TV.setText("15th");
+                break;
+            case R.id.A34:
+                TV.setText("16th");
+                break;
+            case R.id.A41:
+                TV.setText("17th");
+                break;
+            case R.id.A42:
+                TV.setText("18th");
+                break;
+            case R.id.A43:
+                TV.setText("19th");
+                break;
+            case R.id.A44:
+                TV.setText("20th");
+                break;
+            default:
+                TV.setText("Invalid sticker, tap to select another one...");
+                break;
+        }
     }
 }
