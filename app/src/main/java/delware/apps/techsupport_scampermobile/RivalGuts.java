a6c1257f6b87144e4f07a9e4c40305bd11a6c4c7
cjNeumont <rosacj921@gmail.com>
2021-08-20 14:53:21 -0600

    lol

---
+++ app/src/main/java/delware/apps/techsupport_scampermobile/RivalGuts.java
@@ -0,0 +1,7 @@
+package delware.apps.techsupport_scampermobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RivalGuts extends AppCompatActivity {
+    public int difficulty=0;
+}
